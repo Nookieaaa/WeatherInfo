128d55c39e522e0abe06ef47695a04d615b515fd
nookie <nookieaaa@hotmail.com>
2015-12-14 19:24:42 +0200

    Application class for getting static context

--- app/src/main/java/com/nookdev/weathertesttask/MainActivity.java
+++ app/src/main/java/com/nookdev/weathertesttask/MainActivity.java
@@ -1,13 +1,40 @@
 package com.nookdev.weathertesttask;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nookdev.weathertesttask.api.OpenWeatherApi;
+import com.squareup.okhttp.ResponseBody;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String BASE_URL = "http://api.openweathermap.org";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .build();
+        OpenWeatherApi api = retrofit.create(OpenWeatherApi.class);
+        Call <ResponseBody> answer = api.getForecast("kiev");
+        answer.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Response<ResponseBody> response, Retrofit retrofit) {
+                ResponseBody str = response.body();
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/nookdev/weathertesttask/api/OpenWeatherApi.java
+++ app/src/main/java/com/nookdev/weathertesttask/api/OpenWeatherApi.java
@@ -1,6 +1,15 @@
 package com.nookdev.weathertesttask.api;
 
 
+import com.squareup.okhttp.ResponseBody;
+
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
 public interface OpenWeatherApi {
+    @GET("/data/2.5/weather")
+    Call<ResponseBody> getForecast(@Query("q") String city);
+
 }
 
